627de44ae76cdd6cf0680d80e064c93a9480c12c
Fukuda Naoto <naoto.fukuda1217@gmail.com>
2020-07-15 20:17:33 +0900

    Remove unused code

--- backend/shamoji/resources/user.py
+++ backend/shamoji/resources/user.py
@@ -2,26 +2,6 @@ from flask_restful import Resource, reqparse
 from shamoji.models.user import UserModel
 
 
-class Emoji(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument("name", required=True)
-
-    def get(self):
-        emojis = EmojiModel.all()
-        return {"emojis": list(map(lambda emoji: emoji.json(), emojis))}
-
-    def post(self):
-        data = Emoji.parser.parse_args()
-        emoji = EmojiModel(**data)
-
-        try:
-            emoji.save()
-        except:
-            return {"message": "Failed to insert emoji"}, 500
-
-        return emoji.json(), 201
-
-
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument("username", required=True)
